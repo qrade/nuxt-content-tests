79908fc230f1e9d4945b4d7f732e1917f2ac3e98
qrade <corrado.pizzi@cubica.eu>
2023-06-07 10:27:00 +0200

    1 file modified

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <ContentDoc :query="{ path: '/', where: {_locale:locale} }" />
+    <pre>
+    curretn locale: {{ locale }} 
+    current pathWithoutLocale: {{ pathWithoutLocale }}
+    </pre>
+    <ContentDoc
+      :query="{ path: pathWithoutLocale, where: { _locale: locale } }"
+    />
   </div>
 </template>
+
 <script setup>
 const { locale } = useI18n();
 const { path } = useRoute();
-const pathWithoutLocale = path.replace(`/${locale.value}/`, '');
-
-
+const pathWithoutLocale = path.replace(`/${locale.value}`, '/');
 </script>
