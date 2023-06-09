b577fe09a4dd1cb65f6008721bae4379c17851cc
qrade <corrado.pizzi@cubica.eu>
2023-06-09 23:14:44 +0200

    2 files modified

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,17 +1,28 @@
 <template>
   <div>
     <h4>This page is rendered with /pages/[...slug].vue</h4>
-   <h3>These are  vars obtained used the page for the second ContentDoc query (see below):</h3> 
+    <h3>
+      These are vars used in the page for the second &lt;ContentDoc/&gt; query
+      (see below):
+    </h3>
     <pre>
     current locale: {{ locale }} 
-    current pathWithoutLocale: {{ pathWithoutLocale }} (calculated with a regex from path: {{ path }} )
+    current pathWithoutLocale: {{
+        pathWithoutLocale
+      }} (calculated with a regex from path: {{ path }} )
     </pre>
-    <hr/>
-    <h3>This is the content, rendered with a plain ContentDoc tag: (I think it ignore the language doing the query)</h3>
-    <ContentDoc/>
-    <hr/>
+    <hr />
+    <h3>
+      This is the content, rendered with a plain &lt;ContentDoc/&gt; tag: (it
+      ignore the language in the url doing the query!)
+    </h3>
+    <ContentDoc />
+    <hr />
 
-    <h3>This is the content, rendered with a ContentDoc tag enriched with a query:</h3>
+    <h3>
+      This is the content, rendered with a &lt;ContentDoc/&gt; tag enriched with
+      a query with modified path (with _locale code stripped) and added _locale:
+    </h3>
     <ContentDoc
       :query="{ path: pathWithoutLocale, where: { _locale: locale } }"
     />
@@ -21,5 +32,8 @@
 <script setup>
 const { locale } = useI18n();
 const { path } = useRoute();
-const pathWithoutLocale = path.replace(new RegExp(`^/${locale.value}(\/|$)`), '/');
+const pathWithoutLocale = path.replace(
+  new RegExp(`^/${locale.value}(\/|$)`),
+  '/'
+);
 </script>
